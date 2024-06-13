a5a88801e711256df0b5e04d2e69d9a0aa549b56
Xianjie <383134+datastream@users.noreply.github.com>
2024-06-13 17:00:27 +0800

    fix 1.26 build

--- ngx_http_sticky_misc.c
+++ ngx_http_sticky_misc.c
@@ -19,14 +19,8 @@
 /* - fix for 1.11.2 removes include <openssl/md5.h> in ngx_md5.h */
 #define MD5_CBLOCK  64
 #define MD5_LBLOCK  (MD5_CBLOCK/4)
-#define MD5_DIGEST_LENGTH 16
 #define SHA_CBLOCK 64
 #define SHA_DIGEST_LENGTH 20
-
-#ifndef SHA_DIGEST_LENGTH
-#define SHA_CBLOCK 64
-#define SHA_DIGEST_LENGTH 20
-#endif
 
 // /* - bugfix for compiling on sles11 - needs gcc4.6 or later*/
 // #pragma GCC diagnostic ignored "-Wuninitialized"
